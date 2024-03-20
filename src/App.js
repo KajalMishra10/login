fe4378d9b11f9d4ee18b8a99cb32f33b9e19f406
Kajal <84652770+KajalMishra10@users.noreply.github.com>
2024-03-20 16:04:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,99 +1,129 @@
 import "./styles.css";
-import { useState } from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+    
+        
+       
+const App = () => {
+  const [formVisible, setFormVisible] = useState(false);
+  const [formValid, setFormValid] = useState(true);
+  const [inpData, setInpData] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    dob: "",
+  });
+  const modalRef = useRef(null);
 
-export default function App() {
-  const [totalData, setTotalData] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [recordsPerPage] = useState(10);
-  const [currentD, setCurrentD] = useState([]);
   useEffect(() => {
-    getData();
+    const handleOutsideClick = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setFormVisible(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
   }, []);
+  const openForm = () => {
+    setFormVisible(true);
+  };
 
-  useEffect(() => {
-    currentData();
-  }, [currentPage, totalData]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const phoneRegex = /^[0-9]{10}$/;
+    if (!phoneRegex.test(inpData.phone)) {
+      alert("Invalid phone number. Please enter a valid 10-digit phone number");
+      return;
+    }
 
-  const currentData = () => {
-    const lastData = currentPage * recordsPerPage;
-    const firstData = lastData - recordsPerPage;
-    const sliced = totalData.slice(firstData, lastData);
-    setCurrentD(() => {
-      return sliced;
+    const currentDate = new Date();
+    const selectedDate = new Date(inpData.dob);
+    if (selectedDate >= currentDate) {
+      alert("Invalid date of birth. Date of birth cannot be in the future.");
+      return;
+    }
+
+    setInpData({
+      username: "",
+      email: "",
+      phone: "",
+      dob: "",
     });
   };
 
-  async function getData() {
-    try {
-      const response = await fetch(
-        "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
-      );
-      const data = await response.json();
-      setTotalData(data);
-    } catch (error) {
-      alert("failed to fetch data");
-    }
-  }
-
   return (
-    <div className="container">
-      <h1>Employee Data Table</h1>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Role</th>
-          </tr>
-        </thead>
-        <tbody>
-          {currentD.map((data) => (
-            <tr key={data.id}>
-              <td>{data.id}</td>
-              <td>{data.name}</td>
-              <td>{data.email}</td>
-              <td>{data.role}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className="buttonCon">
-        <button
-          className="buttonPN"
-          onClick={() => {
-            setCurrentPage((prev) => {
-              if (prev === 1) {
-                return prev;
-              } else {
-                return prev - 1;
-              }
-            });
-            currentData();
-          }}
-        >
-          Previous
-        </button>
-        <button className="number">{currentPage}</button>
-        <button
-          className="buttonPN"
-          onClick={() => {
-            const final = Math.floor(totalData.length / recordsPerPage) + 1;
-            setCurrentPage((prev) => {
-              if (prev === final) {
-                return prev;
-              } else {
-                return prev + 1;
-              }
-            });
-            currentData();
-          }}
-        >
-          Next
-        </button>
+    <div
+      className="background"
+      style={{ backgroundColor: `${formVisible ? "#7f7f7f" : "#fff"}` }}
+    >
+      <div className={`${formVisible ? "disabled" : ""}`}>
+        <h1>User Details Modal</h1>
+        <button onClick={openForm}>Open Form</button>
       </div>
+
+      {formVisible && (
+        <div ref={modalRef} className="modal">
+          <div className="modal-content">
+            <form onSubmit={handleSubmit}>
+              <h2>Fill Details</h2>
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                value={inpData.username}
+                onChange={(e) => {
+                  setInpData({ ...inpData, username: e.target.value });
+                }}
+                required
+              />
+              <label htmlFor="email">Email Address:</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={inpData.email}
+                onChange={(e) => {
+                  setInpData({ ...inpData, email: e.target.value });
+                }}
+                required
+              />
+              <label htmlFor="phone">Phone Number:</label>
+              <input
+                type="text"
+                name="phone"
+                id="phone"
+                value={inpData.phone}
+                onChange={(e) => {
+                  setInpData({ ...inpData, phone: e.target.value });
+                }}
+                required
+              />
+              <label htmlFor="dob">Date of Birth:</label>
+              <input
+                type="date"
+                name="dob"
+                id="dob"
+                value={inpData.dob}
+                onChange={(e) => {
+                  setInpData({ ...inpData, dob: e.target.value });
+                }}
+                required
+              />
+
+              <button className="submit-button" type="submit">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
+
+export default App;
 
